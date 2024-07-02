2bc46e26bcfaefd6463fe7dbb213f3dc7dd7860e
dewiruntuwene <71383399+dewiruntuwene@users.noreply.github.com>
2024-07-02 09:51:45 +0700

    Update index.js

--- src/pages/Splash/index.js
+++ src/pages/Splash/index.js
@@ -5,7 +5,7 @@ import Logo from '../../assets/icon/Logo.svg';
 const SplashScreen = ({navigation}) => {
   useEffect(() => {
     setTimeout(() => {
-      navigation.replace('PelaporanRelawan');
+      navigation.replace('SignIn');
     }, 3000);
   }, [navigation]);
 
