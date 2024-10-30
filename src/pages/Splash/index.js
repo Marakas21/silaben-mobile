e2c11246fb3b4a6994a0d3f83dc7c4304dd1b7bf
dewiruntuwene <71383399+dewiruntuwene@users.noreply.github.com>
2024-10-31 02:31:21 +0700

    Update index.js

--- src/pages/Splash/index.js
+++ src/pages/Splash/index.js
@@ -5,7 +5,7 @@ import Logo from '../../assets/icon/Logo.svg';
 const SplashScreen = ({navigation}) => {
   useEffect(() => {
     setTimeout(() => {
-      navigation.replace('HomeMasyarakat');
+      navigation.replace('HistoryPelaporan');
     }, 3000);
   }, [navigation]);
 
