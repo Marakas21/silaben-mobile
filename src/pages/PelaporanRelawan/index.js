56998c4b4a4c26465915a5d5c809e7c6d7f6818c
dewiruntuwene <71383399+dewiruntuwene@users.noreply.github.com>
2024-11-05 13:45:05 +0700

    Update index.js

--- src/pages/PelaporanRelawan/index.js
+++ src/pages/PelaporanRelawan/index.js
@@ -11,6 +11,7 @@ import {
   PermissionsAndroid,
   Platform,
   Alert,
+  ImageBackground,
 } from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import DateTimePickerModal from 'react-native-modal-datetime-picker';
@@ -20,7 +21,7 @@ import MapView, {Marker} from 'react-native-maps';
 import LinearGradient from 'react-native-linear-gradient';
 import axios from 'axios';
 import Navbar from '../../components/Navbar';
-import Geolocation from 'react-native-geolocation-service';
+// import Geolocation from 'react-native-geolocation-service';
 
 const Pelaporan = React.memo(
   ({
@@ -276,8 +277,28 @@ const Pelaporan = React.memo(
 
     return (
       <SafeAreaView style={styles.container}>
-        <ScrollView contentContainerStyle={styles.scrollContainer}>
-          <View>
+        <View style={styles.topImageContainer}>
+          <ImageBackground
+            source={require('../../assets/images/bencana-splash.jpg')}
+            style={styles.topImage}
+            imageStyle={styles.imageStyle}>
+            <View style={styles.overlay} />
+            {/* <View style={styles.header}>
+          <Text style={styles.headerText}>SILABEN</Text>
+        </View> */}
+            <Text style={styles.headerText}>Buat Pelaporan</Text>
+            <TouchableOpacity
+              onPress={() => navigation.navigate('HomeMasyarakat')}>
+              <Image
+                source={require('../../../src/assets/images/home_white.png')}
+                style={styles.buttonIcon}
+              />
+            </TouchableOpacity>
+          </ImageBackground>
+        </View>
+        <View style={styles.curvedContainer}>
+          <ScrollView contentContainerStyle={styles.scrollContainer}>
+            {/* <View>
             <LinearGradient
               colors={['#0D85FE', '#003366']}
               style={styles.header}>
@@ -292,194 +313,198 @@ const Pelaporan = React.memo(
                 />
               </TouchableOpacity>
             </LinearGradient>
-          </View>
+          </View> */}
 
-          <View style={styles.inputContainer}>
-            <TextInput
-              style={styles.input}
-              placeholder="Judul Laporan"
-              placeholderTextColor="#707070"
-              value={report_title}
-              onChangeText={text => setReportTitle(text)}
-            />
-            <View style={styles.pickerContainer}>
-              <Picker
-                selectedValue={lapor_instansi}
-                onValueChange={itemValue => setLaporInstansi(itemValue)}
-                style={styles.picker}>
-                <Picker.Item
-                  style={styles.inputText}
-                  label="Segera Lapor ke Instansi Terkait"
-                  value=""
-                />
-                <Picker.Item
-                  style={styles.inputText}
-                  label="Kepolisian"
-                  value="Kepolisian"
-                />
-                <Picker.Item
-                  style={styles.inputText}
-                  label="Rumah Sakit"
-                  value="Rumah Sakit"
-                />
-                <Picker.Item
-                  style={styles.inputText}
-                  label="Kelurahan"
-                  value="Kelurahan"
-                />
-                <Picker.Item
-                  style={styles.inputText}
-                  label="BPBD"
-                  value="BPBD"
-                />
-                <Picker.Item
-                  style={styles.inputText}
-                  label="Pemadam Kebakaran"
-                  value="Pemadam Kebakaran"
-                />
-                <Picker.Item
-                  style={styles.inputText}
-                  label="Tidak Mendesak"
-                  value="Tidak Mendesak"
-                />
-                <Picker.Item
-                  style={styles.inputText}
-                  label="Lainnya"
-                  value="Lainnya"
-                />
-              </Picker>
-            </View>
-            <TouchableOpacity onPress={showDatePicker} style={styles.dateInput}>
-              <Text
-                style={styles.inputText}
-                value={date}
-                onChangeText={text => setDate(text)}>
-                {date ? date : 'Tanggal Kejadian Bencana'}
-              </Text>
-              <Image
-                source={require('../../../src/assets/images/calendar.png')}
-                style={styles.icon}
+            <View style={styles.inputContainer}>
+              <TextInput
+                style={styles.input}
+                placeholder="Judul Laporan"
+                placeholderTextColor="#707070"
+                value={report_title}
+                onChangeText={text => setReportTitle(text)}
               />
-            </TouchableOpacity>
-            <TouchableOpacity onPress={showTimePicker} style={styles.dateInput}>
-              <Text
-                style={styles.inputText}
-                value={time}
-                onChangeText={text => setTime(text)}>
-                {time ? time : 'Waktu Kejadian Bencana'}
-              </Text>
-              <Image
-                source={require('../../../src/assets/images/clock.png')}
-                style={styles.icon}
+              <View style={styles.pickerContainer}>
+                <Picker
+                  selectedValue={lapor_instansi}
+                  onValueChange={itemValue => setLaporInstansi(itemValue)}
+                  style={styles.picker}>
+                  <Picker.Item
+                    style={styles.inputText}
+                    label="Segera Lapor ke Instansi Terkait"
+                    value=""
+                  />
+                  <Picker.Item
+                    style={styles.inputText}
+                    label="Kepolisian"
+                    value="Kepolisian"
+                  />
+                  <Picker.Item
+                    style={styles.inputText}
+                    label="Rumah Sakit"
+                    value="Rumah Sakit"
+                  />
+                  <Picker.Item
+                    style={styles.inputText}
+                    label="Kelurahan"
+                    value="Kelurahan"
+                  />
+                  <Picker.Item
+                    style={styles.inputText}
+                    label="BPBD"
+                    value="BPBD"
+                  />
+                  <Picker.Item
+                    style={styles.inputText}
+                    label="Pemadam Kebakaran"
+                    value="Pemadam Kebakaran"
+                  />
+                  <Picker.Item
+                    style={styles.inputText}
+                    label="Tidak Mendesak"
+                    value="Tidak Mendesak"
+                  />
+                  <Picker.Item
+                    style={styles.inputText}
+                    label="Lainnya"
+                    value="Lainnya"
+                  />
+                </Picker>
+              </View>
+              <TouchableOpacity
+                onPress={showDatePicker}
+                style={styles.dateInput}>
+                <Text
+                  style={styles.inputText}
+                  value={date}
+                  onChangeText={text => setDate(text)}>
+                  {date ? date : 'Tanggal Kejadian Bencana'}
+                </Text>
+                <Image
+                  source={require('../../../src/assets/images/calendar.png')}
+                  style={styles.icon}
+                />
+              </TouchableOpacity>
+              <TouchableOpacity
+                onPress={showTimePicker}
+                style={styles.dateInput}>
+                <Text
+                  style={styles.inputText}
+                  value={time}
+                  onChangeText={text => setTime(text)}>
+                  {time ? time : 'Waktu Kejadian Bencana'}
+                </Text>
+                <Image
+                  source={require('../../../src/assets/images/clock.png')}
+                  style={styles.icon}
+                />
+              </TouchableOpacity>
+              <DateTimePickerModal
+                isVisible={isTimePickerVisible}
+                mode="time"
+                onConfirm={handleConfirmTime}
+                onCancel={hideTimePicker}
               />
-            </TouchableOpacity>
-            <DateTimePickerModal
-              isVisible={isTimePickerVisible}
-              mode="time"
-              onConfirm={handleConfirmTime}
-              onCancel={hideTimePicker}
-            />
-            <DateTimePickerModal
-              isVisible={isDatePickerVisible}
-              mode="date"
-              onConfirm={handleConfirm}
-              onCancel={hideDatePicker}
-            />
-            <TextInput
-              style={styles.input}
-              placeholder="Deskripsi bencana..."
-              value={description}
-              onChangeText={setDescription}
-              placeholderTextColor="#707070"
-            />
-            <TextInput
-              style={styles.input}
-              placeholder="Cari Lokasi"
-              value={searchText}
-              onChangeText={setSearchText}
-              onSubmitEditing={searchLocation}
-              placeholderTextColor="#707070"
-            />
-            <View style={styles.mapContainer}>
-              <MapView
-                style={styles.map}
-                onPress={handleMapPress}
-                initialRegion={{
-                  latitude: 1.4153965,
-                  longitude: 124.9867153,
-                  latitudeDelta: 0.0922,
-                  longitudeDelta: 0.0421,
-                }}
-                region={{
-                  latitude: location.latitude,
-                  longitude: location.longitude,
-                  latitudeDelta: 0.0922,
-                  longitudeDelta: 0.0421,
-                }}>
-                {location.latitude !== 0 && (
-                  <Marker
-                    coordinate={{
-                      latitude: location.latitude,
-                      longitude: location.longitude,
-                    }}
-                    draggable
-                    onDragEnd={handleMarkerDragEnd}
+              <DateTimePickerModal
+                isVisible={isDatePickerVisible}
+                mode="date"
+                onConfirm={handleConfirm}
+                onCancel={hideDatePicker}
+              />
+              <TextInput
+                style={styles.input}
+                placeholder="Deskripsi bencana..."
+                value={description}
+                onChangeText={setDescription}
+                placeholderTextColor="#707070"
+              />
+              <TextInput
+                style={styles.input}
+                placeholder="Cari Lokasi"
+                value={searchText}
+                onChangeText={setSearchText}
+                onSubmitEditing={searchLocation}
+                placeholderTextColor="#707070"
+              />
+              <View style={styles.mapContainer}>
+                <MapView
+                  style={styles.map}
+                  onPress={handleMapPress}
+                  initialRegion={{
+                    latitude: 1.4153965,
+                    longitude: 124.9867153,
+                    latitudeDelta: 0.0922,
+                    longitudeDelta: 0.0421,
+                  }}
+                  region={{
+                    latitude: location.latitude,
+                    longitude: location.longitude,
+                    latitudeDelta: 0.0922,
+                    longitudeDelta: 0.0421,
+                  }}>
+                  {location.latitude !== 0 && (
+                    <Marker
+                      coordinate={{
+                        latitude: location.latitude,
+                        longitude: location.longitude,
+                      }}
+                      draggable
+                      onDragEnd={handleMarkerDragEnd}
+                    />
+                  )}
+                </MapView>
+              </View>
+              <View style={styles.locationTextContainer}>
+                <Text
+                  style={styles.locationText}
+                  value={input_lokasi_bencana}
+                  onChangeText={text => setLocationText(text)}>
+                  {location.name}
+                </Text>
+              </View>
+              <View style={styles.locationInputContainer}>
+                <View style={styles.inputLanLong}>
+                  <Text style={styles.label}>Latitude:</Text>
+                  <TextInput
+                    style={styles.input}
+                    value={location.latitude.toString()}
+                    onChangeText={text =>
+                      setLocation({...location, latitude: parseFloat(text)})
+                    }
+                    keyboardType="numeric"
+                  />
+                </View>
+                <View style={styles.inputLanLong}>
+                  <Text style={styles.label}>Longitude:</Text>
+                  <TextInput
+                    style={styles.input}
+                    value={location.longitude.toString()}
+                    onChangeText={text =>
+                      setLocation({...location, longitude: parseFloat(text)})
+                    }
+                    keyboardType="numeric"
+                  />
+                </View>
+              </View>
+              <TextInput
+                style={styles.input}
+                placeholder={locationText}
+                placeholderTextColor="#707070"
+              />
+              <TouchableOpacity
+                onPress={handleImagePicker}
+                style={styles.imageInput}>
+                <Text style={styles.inputText}>Unggah Gambar</Text>
+                {selectedImage && (
+                  <Image
+                    source={{uri: selectedImage.uri}}
+                    style={styles.imagePreview}
                   />
                 )}
-              </MapView>
-            </View>
-            <View style={styles.locationTextContainer}>
-              <Text
-                style={styles.locationText}
-                value={input_lokasi_bencana}
-                onChangeText={text => setLocationText(text)}>
-                {location.name}
-              </Text>
-            </View>
-            <View style={styles.locationInputContainer}>
-              <View style={styles.inputLanLong}>
-                <Text style={styles.label}>Latitude:</Text>
-                <TextInput
-                  style={styles.input}
-                  value={location.latitude.toString()}
-                  onChangeText={text =>
-                    setLocation({...location, latitude: parseFloat(text)})
-                  }
-                  keyboardType="numeric"
-                />
-              </View>
-              <View style={styles.inputLanLong}>
-                <Text style={styles.label}>Longitude:</Text>
-                <TextInput
-                  style={styles.input}
-                  value={location.longitude.toString()}
-                  onChangeText={text =>
-                    setLocation({...location, longitude: parseFloat(text)})
-                  }
-                  keyboardType="numeric"
-                />
-              </View>
-            </View>
-            <TextInput
-              style={styles.input}
-              placeholder={locationText}
-              placeholderTextColor="#707070"
-            />
-            <TouchableOpacity
-              onPress={handleImagePicker}
-              style={styles.imageInput}>
-              <Text style={styles.inputText}>Unggah Gambar</Text>
-              {selectedImage && (
-                <Image
-                  source={{uri: selectedImage.uri}}
-                  style={styles.imagePreview}
-                />
-              )}
-            </TouchableOpacity>
-            {fileName ? (
-              <Text style={styles.label}>Nama File: {fileName}</Text>
-            ) : null}
-            {/* <View style={styles.radioGroup}>
+              </TouchableOpacity>
+              {fileName ? (
+                <Text style={styles.label}>Nama File: {fileName}</Text>
+              ) : null}
+              {/* <View style={styles.radioGroup}>
               <TouchableOpacity
                 style={styles.radioButton}
                 onPress={() => setAnonim('Anonim')}>
@@ -505,18 +530,19 @@ const Pelaporan = React.memo(
                 <Text style={styles.radioText}>Tidak Anonim</Text>
               </TouchableOpacity>
             </View> */}
-            <TouchableOpacity
-              style={styles.button}
-              onPress={handleCreatePelaporan}>
-              <LinearGradient
-                colors={['#0D85FE', '#0000FF']}
-                style={styles.buttonBackground}
+              <TouchableOpacity
+                style={styles.button}
                 onPress={handleCreatePelaporan}>
-                <Text style={styles.buttonText}>Laporkan</Text>
-              </LinearGradient>
-            </TouchableOpacity>
-          </View>
-        </ScrollView>
+                <LinearGradient
+                  colors={['#0D85FE', '#0000FF']}
+                  style={styles.buttonBackground}
+                  onPress={handleCreatePelaporan}>
+                  <Text style={styles.buttonText}>Laporkan</Text>
+                </LinearGradient>
+              </TouchableOpacity>
+            </View>
+          </ScrollView>
+        </View>
         <Navbar navigation={navigation} />
       </SafeAreaView>
     );
@@ -532,23 +558,55 @@ const styles = StyleSheet.create({
     flexGrow: 1,
   },
   header: {
-    flexDirection: 'row',
+    backgroundColor: '#003366',
+    padding: 30,
     alignItems: 'center',
+    borderBottomRightRadius: 5,
+    borderBottomLeftRadius: 5,
+    flexDirection: 'row',
     justifyContent: 'space-between',
-    paddingHorizontal: 20,
-    paddingVertical: 15,
+    marginBottom: 30,
   },
   headerText: {
-    color: '#fff',
+    color: '#FFFFFF',
     fontSize: 18,
+    fontWeight: 'bold',
+    flex: 1,
+    padding: 35,
+  },
+  curvedContainer: {
+    backgroundColor: 'white',
+    marginTop: -20, // Adjust to overlap with the top image
+    borderTopLeftRadius: 30, // Apply a curve to the top-left corner
+    borderTopRightRadius: 30, // Apply a curve to the top-right corner
+    padding: 20,
+  },
+  topImage: {
+    width: '100%',
+    height: 100,
+    flexDirection: 'row',
+  },
+  imageStyle: {
+    resizeMode: 'cover',
+  },
+  overlay: {
+    ...StyleSheet.absoluteFillObject,
+    backgroundColor: '#0066CC',
+    opacity: 0.5, // Adjust the opacity as needed
   },
   buttonIcon: {
     width: 24,
     height: 24,
-    alignItems: 'right',
+    marginTop: 35,
+    marginRight: 35,
   },
   inputContainer: {
-    padding: 20,
+    overflow: 'hidden', // Ensures content doesn't exceed rounded corners
+    backgroundColor: '#fff', // Background color of the report section
+    padding: 10,
+    // eslint-disable-next-line no-dupe-keys
+    overflow: 'hidden',
+    marginBottom: 200,
   },
   input: {
     borderBottomWidth: 1,
