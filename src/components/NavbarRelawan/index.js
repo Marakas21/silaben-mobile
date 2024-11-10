278690cc231ef5cf0e659c63030713694f0e8454
dewiruntuwene <71383399+dewiruntuwene@users.noreply.github.com>
2024-11-11 02:43:14 +0700

    Create index.js

---
+++ src/components/NavbarRelawan/index.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import {View, Text, StyleSheet, TouchableOpacity, Image} from 'react-native';
+import {useNavigation} from '@react-navigation/native';
+
+const Navbar = () => {
+  const navigation = useNavigation();
+
+  return (
+    <View style={styles.navbar}>
+      <TouchableOpacity
+        style={styles.navButton}
+        onPress={() => navigation.navigate('MapRelawan')}>
+        <Image
+          source={require('../../../src/assets/images/maps2.png')}
+          style={styles.navIcon}
+        />
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={styles.navButton}
+        onPress={() => navigation.navigate('PelaporanRelawan')}>
+        <Image
+          source={require('../../../src/assets/images/add_report2.png')}
+          style={styles.navIcon}
+        />
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={styles.navButton}
+        onPress={() => navigation.navigate('ProfileRelawan')}>
+        <Image
+          source={require('../../../src/assets/images/profile_pict3.png')}
+          style={styles.navIcon}
+        />
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  navbar: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    padding: 15,
+    borderTopWidth: 1,
+    borderColor: '#EEEEEE',
+    backgroundColor: '#FFFFFF',
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    right: 0,
+  },
+  navButton: {
+    alignItems: 'center',
+  },
+  navIcon: {
+    width: 35,
+    height: 30,
+  },
+});
+
+export default Navbar;
